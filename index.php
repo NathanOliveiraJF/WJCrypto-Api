e1c80cf1f7fc4d1992a838a6ba4b8394075beba7
Nathan Oliveira <nathanoliveira003jf@gmail.com>
2024-03-03 23:45:29 -0300

    chore: require config iluminate and config env package

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 require_once "vendor/autoload.php";
-require_once "config/orm.php";
+require_once "config/illuminate-database.config.php";
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
