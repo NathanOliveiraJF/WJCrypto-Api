1a65452ec124dd0a3f6c2d8d479686cfdd6f9cb2
Nathan Oliveira <nathanoliveira003jf@gmail.com>
2024-03-03 23:45:00 -0300

    chore: added config database orm illuminate

---
+++ config/illuminate-database.config.php
@@ -0,0 +1,16 @@
+<?php
+require_once "vendor/autoload.php";
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+$capsule = new Capsule();
+
+$capsule->addConnection([
+    'driver' => 'mysql',
+    'host' => $_ENV['DB_HOST'],
+    'database' =>  $_ENV['DB_DATABASE'],
+    'username' => $_ENV['DB_USER'],
+    'password' =>  $_ENV['DB_PASS'],
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+]);
