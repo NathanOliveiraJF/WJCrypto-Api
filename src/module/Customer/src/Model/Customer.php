2f11c87d12a7b0dd40e1ae2986bcc596c2b497f4
Nathan Oliveira <nathanoliveira003jf@gmail.com>
2024-03-03 23:56:58 -0300

    refactor: change structure module

--- src/module/Customer/src/Model/Customer.php
+++ src/module/Customer/src/Model/Customer.php
@@ -5,36 +5,14 @@ declare(strict_types=1);
 namespace Webjump\Customer\Model;
 
 use DateTime;
-use Doctrine\DBAL\Types\Types;
-use Doctrine\ORM\Mapping\Column;
-use Doctrine\ORM\Mapping\Entity;
-use Doctrine\ORM\Mapping\GeneratedValue;
-use Doctrine\ORM\Mapping\Id;
-use Doctrine\ORM\Mapping\Table;
 
-#[Entity]
-#[Table(name: 'customer')]
 class Customer  {
-  #[Id]
-  #[Column(type: Types::INTEGER)]
-  #[GeneratedValue()]
   private int $entityId;
-
   private string $name;
-
-  #[Column(length: 15)]
   private string $taxvat;
-
-  #[Column(length: 50, unique: true)]
   private string $docNumber;
-
-  #[Column(length: 40)]
   private string $telephone;
-
-  #[Column(length: 40)]
   private string $address;
-
-  #[Column(name: 'dob', type: Types::DATETIME_MUTABLE)]
   private DateTime $dob;
 
   public function __construct(int $entityId, string $name, string $taxvat, string $docnumber, string $telephone, string $address, DateTime $dob) {
